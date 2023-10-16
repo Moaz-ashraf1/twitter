6d9fbc472817f0e1069c782dd9dd310816a5ce20
moaz ashraf <moaza2298@gmail.com>
2023-10-16 23:31:02 +0300

    bcrypt password before save it in database

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+const bcrypt= require('bcryptjs')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -27,5 +27,8 @@ const userSchema = new mongoose.Schema({
       },
 
 },{timestamps:true })
-
+userSchema.pre('save',async function(next){
+  this.password = await bcrypt.hash(this.password ,12);
+  next();
+})
 module.exports = mongoose.model("User",userSchema)
